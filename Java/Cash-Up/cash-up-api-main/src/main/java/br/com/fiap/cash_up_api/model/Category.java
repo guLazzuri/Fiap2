1147847062a8f6c08e58bc186283f6529e81578f
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2025-03-26 09:51:08 -0300

    Java

---
+++ Java/Cash-Up/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/model/Category.java
@@ -0,0 +1,37 @@
+package br.com.fiap.cash_up_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String icon;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " - " + icon;
+    }
+
+}
--- Java/cash-up-api/src/main/java/br/com/fiap/cash_up_api/validation/validation.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.cash_up_api.validation;
-
-public class validation {
-    
-    //Fizemos e validação no model.
-}
